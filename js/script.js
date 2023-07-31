ae1b650c4ff9a4d573c637d8ab737ba48126583b
David Curras <david.curras@radiumrocket.com>
2023-07-31 13:32:30 -0300

    JS Keydown listener

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 var pacman;
 var ghost;
+var cookie;
 
-window.addEventListener('DOMContentLoaded', function(){
+var movePacman = function (evt) {
+    // @see https://developer.mozilla.org/en-US/docs/Web/API/Element/keydown_event
+    if (evt.isComposing || evt.keyCode === 229) return;
+    // @see https://developer.mozilla.org/en-US/docs/Web/API/UIEvent/which
+    console.log(evt.key, evt.code, evt.which);
+};
+
+var init = function() {
     pacman = document.getElementById('pacman');
     ghost = document.getElementById('ghost');
-    console.log(pacman);
-    console.log(ghost);
-});
+    cookie = document.getElementById('cookie');
+    document.addEventListener('keydown', movePacman);
+};
+
+window.addEventListener('DOMContentLoaded', init);
